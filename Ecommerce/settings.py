02c8b6f64e0321f5a65820e4ed5b83edc4f34a43
athallahw <athallahwibowo18@gmail.com>
2024-09-19 22:14:31 +0700

    deploy pws

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8qzbr#qiah+_hj$67+#dv=38wirqrzg$4_jxbqoiw%^0-d4#$w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "athallah.wibowo-Ecommerce.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "athallah.wibowo-Ecommerce.pbp.cs.ui.ac.id", "athallah-wibowo-tokokuning.pbp.cs.ui.ac.id"]
 
 
 # Application definitions
